a184f7a200b29c9c1bc6a2d13c2341aeef625931
Arthur Queiroz <arthurqueiroz25@gmail.com>
2025-08-25 02:39:48 -0300

    initial commit.

---
+++ src/Application/DependencyInjection/Swagger/Registration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Application.DependencyInjection.Swagger;
+
+public static class Registration
+{
+    public static void AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo { Title = "NyTimesApi", Version = "v1" });
+
+            var securityScheme = new OpenApiSecurityScheme
+            {
+                Name = "Authorization",
+                Description = "Insira o token JWT no formato: Bearer {seu token}",
+                In = ParameterLocation.Header,
+                Type = SecuritySchemeType.Http,
+                Scheme = "bearer",
+                BearerFormat = "JWT",
+                Reference = new OpenApiReference { Type = ReferenceType.SecurityScheme, Id = "Bearer" }
+            };
+
+            options.AddSecurityDefinition("Bearer", securityScheme);
+
+            var securityRequirement = new OpenApiSecurityRequirement { { securityScheme, ["Bearer"] } };
+
+            options.AddSecurityRequirement(securityRequirement);
+        });
+    }
+}
